c897ed21b91d48711ce317ca70ed1cf8ea4014c5
LeeChoongHwan <choo901@naver.com>
2025-04-08 10:49:54 +0900

    complete day1

---
+++ Day1_PythonBasic/day1_project.py
@@ -0,0 +1,3 @@
+city_name = input("What's the name of the city you grew up in?")
+pet_name = input("What's your pet's name?")
+print("Your band name could be" + " " + city_name + " " + pet_name)
---
+++ Day1_PythonBasic/input.py
@@ -0,0 +1,9 @@
+#input을 위한 함수, Java의 scanner와 유사
+name = input("What's your name?")
+print(len(name))
+print(len(name.replace(' ','')))
+#len 글자수 카운트 함수
+
+# python은 변수 이름에서 cammel case를 사용하지 않는다. / snake case 사용 / 가장 앞에 숫자가 올수 없다.
+
+name_of_user = "충환"
